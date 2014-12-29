033afc47bc74a3d0944092a4b40ba177289bc34d
goke-epapa <adegokeobasa@gmail.com>
2014-12-29 09:49:58 +0100

    Added files for Lesson 4a and 4b

--- app/src/main/java/me/adegokeobasa/sunshine/app/MainActivity.java
+++ app/src/main/java/me/adegokeobasa/sunshine/app/MainActivity.java
@@ -23,8 +23,38 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new ForecastFragment())
                     .commit();
         }
+        Log.d(TAG, "onCreate");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
